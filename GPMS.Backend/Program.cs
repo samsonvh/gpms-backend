f66caf58ced1bfe9977816575731c8bbdfe71a0c
Kun <phamquangthang119@gmail.com>
2024-07-24 14:40:02 +0700

    Update cors in program.cs

--- GPMS.Backend/Program.cs
+++ GPMS.Backend/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(MyAllowSpecificOrigins, builder =>
     {
-        builder.WithOrigins("http://localhost:5173/", "http://localhost:5174/", "http://localhost:3000", "https://gpms-frontend-samsonvhs-projects.vercel.app/", "https://rpms-web.vercel.app/")
+        builder.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://gpms-frontend-samsonvhs-projects.vercel.app/", "https://rpms-web.vercel.app/")
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
